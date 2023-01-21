0e9d8ad8ff92bde333d9e3a0802d861c6090a52b
Felipe Dias <felipemacdowell@unifor.br>
2023-01-21 00:05:25 -0300

    Aula02

---
+++ web/src/components/HabitDay.tsx
@@ -0,0 +1,8 @@
+interface HabitDayProps {
+}
+
+export default function HabitDay(props: HabitDayProps) {
+  return (
+    <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+  )
+}
